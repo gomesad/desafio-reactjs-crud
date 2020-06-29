b33265e7d5cc277aa7020eb250888f109e2e3d09
Anderson Figueredo Gomes <gomes.ad@gmail.com>
2020-06-29 12:57:53 -0300

    innitial commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import Header from '../../components/Header';
 
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      api.get('/foods').then(response => {
+        setFoods(response.data);
+      });
     }
 
     loadFoods();
@@ -38,6 +40,10 @@ const Dashboard: React.FC = () => {
   ): Promise<void> {
     try {
       // TODO ADD A NEW FOOD PLATE TO THE API
+      await api.post('/foods', food).then(response => {
+        const foodPlate = response.data;
+        setFoods([...foods, foodPlate]);
+      });
     } catch (err) {
       console.log(err);
     }
@@ -47,10 +53,29 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     // TODO UPDATE A FOOD PLATE ON THE API
+    const foodPlate = {
+      id: editingFood.id,
+      name: food.name,
+      image: food.image,
+      price: food.price,
+      description: food.description,
+      available: editingFood.available,
+    } as IFoodPlate;
+
+    await api.put(`/foods/${foodPlate.id}`, foodPlate);
+
+    const newFoods = foods.map(f => (f.id === foodPlate.id ? foodPlate : f));
+
+    setFoods(newFoods);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    const newFoods = foods.filter(food => food.id !== id);
+
+    setFoods(newFoods);
   }
 
   function toggleModal(): void {
@@ -63,6 +88,8 @@ const Dashboard: React.FC = () => {
 
   function handleEditFood(food: IFoodPlate): void {
     // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    toggleEditModal();
+    setEditingFood(food);
   }
 
   return (
